e75d1ef1a7b3a1653b4345a4107ad41110b1f31b
Luis Gomez <luiskaco@gmail.com>
2022-02-01 13:34:42 -0500

    Corregido errores de galeria

--- resources/views/layouts/modals/modalPermiso.blade.php
+++ resources/views/layouts/modals/modalPermiso.blade.php
@@ -3,26 +3,26 @@
 role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-        <div class="modal-header">
-            <h4 class="modal-title font-weight-bold text-pink headingPermit">CONSENTIMIENTO DE DATOS PERSONALES</h4>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-
-            <div class="contentPermitGrid">
-                <embed src="{{asset('/pdf/terminos.pdf')}}"
-                type="application/pdf">
+            <div class="modal-header">
+                <h4 class="modal-title font-weight-bold text-pink headingPermit">CONSENTIMIENTO DE DATOS PERSONALES</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <div class="modal-body">
+
+                <div class="contentPermitGrid">
+                    <embed src="{{asset('/pdf/terminos.pdf')}}"
+                    type="application/pdf">
+                </div>
 
 
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary custom-button" data-dismiss="modal">Cerrar</button>
+
+            </div>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary custom-button" data-dismiss="modal">Cerrar</button>
-
-        </div>
-    </div>
     </div>
 
     <style>
--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -48,31 +48,94 @@
 
                 </div>
             </div>
-            <div class="instagram-card">
-                <div class="instagram-card-header">
-                    <span class="instagram-card-user-name">Video 1</span>
-                </div> <div class="instagram-card-image">
-                    <iframe width="330" height="215" src="https://www.youtube.com/embed/heDapKxMr7s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen">
-
-                    </iframe>
-                    <div class="instagram-card-content">
-                        <p class="card-description"></p>
-                    </div>
-                </div>
-                <div class="instagram-card-footer">
-
-                </div>
-            </div>
-
-
 
         </div>
     </div>
 </div>
 
-
+<div class="space-10">
+</div>
 @endsection
 
 @section('css')
 <link href="{{asset('css/cardSocial.css')}}" rel="stylesheet">
+
+<style>
+    /*VIDEO STILOS*/
+.container-custom-eventos{
+    padding-top: 30px !important;
+}
+
+.instagram-card-image iframe{
+    width: 100%;
+    height: auto;
+
+}
+
+@media(min-width:768px){
+    .instagram-card-image iframe{
+      height: 300px;
+    }
+}
+
+hr {
+border-bottom: 0px !important;
+}
+
+.container-cards {
+grid-template-columns: repeat(2, 1fr) !important;
+}
+
+.instagram-card-image:after {
+position: unset !important;
+}
+
+.btn:focus {
+box-shadow: unset !important;
+}
+
+@media screen and (max-width:1024px){
+
+    .container{
+            padding: 0 !important;
+            margin: 0;
+        }
+
+    .container-custom-eventos{
+        padding: 30px 0 60px !important;
+    }
+
+    /* NO BORRAR ES UNA EXCEPCION PARA QUE PUEDA FUNCIONAR EN ESTA PESTAÑA */
+            footer, .container-footer{
+            position: unset !important;
+        }
+}
+
+@media screen and (max-width:768px){
+
+    .container-cards {
+        grid-template-columns: repeat(2, 1fr) !important;
+    }
+}
+
+@media screen and (max-width:550px){
+    .container-custom-eventos{
+        padding: 30px 15px 15px !important;
+    }
+
+    .headingSocial{
+        margin: auto !important;
+    }
+
+    .headingSocial p {
+        font-size: .9rem !important;
+    }
+    .container-cards {
+        grid-template-columns: repeat(1, 1fr) !important;
+    }
+
+}
+
+</style>
+
 @endsection
